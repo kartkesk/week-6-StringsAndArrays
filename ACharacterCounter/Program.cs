d03a0730944fb7b22155d8cad4ffa70db051ca28
Kärt Kesküla <kart.keskula@thermory.com>
2022-03-05 17:16:18 +0200

    add new projects

---
+++ ACharacterCounter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ACharacterCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanime
+            //programm kuvab, mitu 'a' tähte on kasutaja ees- ja perekonnanimeskokku
+
+            Console.WriteLine("Palun, sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Palun, sisesta perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            string fullName = $"{firstName}{lastName}".ToLower();
+
+            Console.WriteLine(fullName);
+
+
+            int aCounter = 0;
+
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                if (fullName[i] == 'a')
+                {
+                    aCounter++;
+                }
+            }
+
+            if (aCounter != 1)
+            {
+                Console.WriteLine($"Sinu täisnimes on {aCounter} 'a' tähte.");
+            }
+            else
+            {
+                Console.WriteLine($"Sinu täisnimes on {aCounter} 'a' täht.");
+            }
+
+
+        }
+    }
+}
